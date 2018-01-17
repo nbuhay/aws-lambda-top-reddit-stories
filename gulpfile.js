78b2bb549724387142034e4e82dd08c39738c4df
Nick Buhay <nbuhay@gmail.com>
2018-01-17 03:02:02 -0600

    Refactor gulpfile task deploy to both zip and upload code to S3.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,37 @@
 const gulp = require('gulp')
     , filter = require('gulp-filter')
     , zip = require('gulp-zip')
+    , awspublish = require('gulp-awspublish')
+    , AWS = require('aws-sdk')
+    , S3 = new AWS.S3()
 
 gulp.task('default', () => console.log('Hello from aws-lambda-top-stories gulpfile!'));
 
-gulp.task('zip', () => {
-  // include all files and folders in build
-  // include all files that start with a . i.e. .config, .gitignore, etc.
-  // exclude existing .zip from this build - it will be overwritten in the pipe
-  const file = 'awsLambdaTopStories-lambdaFunction.js' 
-      , opts = { nodir: true, dot: true }
-      , f = filter(['**', '!deploy/*.zip'])
+gulp.task('deploy', () => {
+  const zipFile = 'aws-lambda-top-stories.deploy.zip'
+      , s3Parmas = { Bucket: process.env.S3_BUCKET, Key: zipFile }
 
-  return gulp.src(file, opts)
-    .pipe(f)
-    .pipe(zip('aws-lambda-top-stories.deploy.zip'))
-    .pipe(gulp.dest('./deploy'));
+  return new Promise((resolve, reject) => {
+    S3.deleteObject(s3Parmas, (err, data) => (err) ? reject(err) : resolve())
+  })
+  .then(() => {
+    // include all files and folders in build
+    // include all files that start with a . i.e. .config, .gitignore, etc.
+    // exclude existing .zip from this build - it will be overwritten in the pipe
+    const jsFile = 'awsLambdaTopStories-lambdaFunction.js' 
+        , opts = { nodir: true, dot: true }
+        , f = filter(['**', '!deploy/*.zip'])
+        , publishParmas = { params: { Bucket: process.env.S3_BUCKET } }
+        , headers = { 'Cache-Control': 'max-age=315360000, no-transform, public' }
+        , publisher = awspublish.create(publishParmas)
+
+    return gulp.src(jsFile, opts)
+      .pipe(f)
+      .pipe(zip(zipFile))
+      .pipe(gulp.dest('./deploy'))
+      .pipe(publisher.publish(headers))
+      .pipe(publisher.sync())
+      .pipe(awspublish.reporter());
+  })
+  .catch((err) => console.error(err));
 });
-
-gulp.task('deploy', () => console.log('Deploy to Lambda'));
